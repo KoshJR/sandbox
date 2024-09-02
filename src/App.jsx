63ec902ae9237fd9d7d238ece0b6cf871d9b797e
Valentyn Koshmanyuk <goldenarm31.03@gmail.com>
2024-09-02 15:15:44 -0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Grid } from "./pages/Grid/Grid";
 import { Home } from "./pages/Home/Home";
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
@@ -12,13 +11,11 @@ function App() {
   return (
     <div className="container">
       <div className="content-wrapper">
-        
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/grid" element={<Grid />} />
+            <Route path="grid" element={<Grid />} />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
-      
       </div>
     </div>
   );
