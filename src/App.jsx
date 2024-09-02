3218f8066bb465f1fbc265670c286d95fd35d695
Valentyn Koshmanyuk <goldenarm31.03@gmail.com>
2024-09-02 12:08:25 -0400

    directory-init

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Grid } from "./pages/Grid/Grid";
+import { Home } from "./pages/Home/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./styles/index.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="content-wrapper">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/grid" element={<Grid />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+      <div>
+          <h1>Welcome to your Sandbox</h1>
+    </div>
+  )
+}
+
+export {Home}
